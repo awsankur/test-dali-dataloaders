f7318532a97605371e773bd941e264eff673f78d
EC2 Default User <ec2-user@ip-172-31-14-101.us-west-2.compute.internal>
2023-07-23 20:03:45 +0000

    Updated

--- multi-channel-images/docker/test-dali-dataloader.py
+++ multi-channel-images/docker/test-dali-dataloader.py
@@ -13,41 +13,39 @@ import glob
 import multiprocessing as mp
 
 
-IMG_DIR = './data/data/DeepPhenotype_PBMC_ImageSet_YSeverin/Training/'
+IMG_DIR = '/data/data/DeepPhenotype_PBMC_ImageSet_YSeverin/Training/'
 TRAIN_BS = 30
-#NUM_WORKERS = 0
-#NUM_IMAGES = 10591
-#89572
 
 multichannel_tiff_files = glob.glob(IMG_DIR + '/**/*.tiff', recursive=True)
 
-#import pdb;pdb.set_trace()
+for num_threads in range(8):
+    # From https://github.com/NVIDIA/DALI/blob/main/dali/test/python/test_pipeline_multichannel.py
+    @pipeline_def(num_threads=num_threads, device_id=0)
+    def get_dali_pipeline():
 
-# From https://github.com/NVIDIA/DALI/blob/main/dali/test/python/test_pipeline_multichannel.py
-@pipeline_def(num_threads=4, device_id=0)
-def get_dali_pipeline():
+        encoded, label = fn.readers.file(files=multichannel_tiff_files, name="Reader")
+        decoded = fn.experimental.decoders.image(encoded, device="mixed", output_type=types.ANY_DATA)
 
-    encoded, label = fn.readers.file(files=multichannel_tiff_files, name="Reader")
-    decoded = fn.experimental.decoders.image(encoded, device="mixed", output_type=types.ANY_DATA)
-
-    images = decoded.gpu()
-    images = fn.resize(images,resize_y=900, resize_x=300)
-    images = fn.crop(images,crop_h=220, crop_w=224,crop_pos_x=0.3, crop_pos_y=0.2)
-    #images = fn.transpose(images,perm=(1, 0, 2),transpose_layout=False)
-    #images = fn.crop_mirror_normalize(images,std=255., mean=0.,output_layout="HWC",dtype=types.FLOAT)
+        images = decoded.gpu()
+        images = fn.resize(images,resize_y=220, resize_x=224)
+        images = fn.crop(images,crop_h=220)
 
     return images, label
 
-train_loader = DALIGenericIterator(
-    [get_dali_pipeline(batch_size=TRAIN_BS)],
-    ['data', 'label'],
-    reader_name='Reader'
-)
+    train_loader = DALIGenericIterator(
+        [get_dali_pipeline(batch_size=TRAIN_BS)],
+        ['data', 'label'],
+        reader_name='Reader'
+    )
 
-start = time.time()
-for epoch in range(1, 3):
-    for i, data in enumerate(train_loader, 0):
-        pass
+    num_epochs = 3
+    start = time.time()
+    for epoch in range(num_epochs):
+        for i, data in enumerate(train_loader, 0):
+            pass
     end = time.time()
-print("Finish with:{} second".format(end - start))
+    time_taken_secs = end - start
+    avg_time_per_epoch_secs = time_taken_secs/num_epochs
+    print("Dali Avg time per epoch:{} second, num_threads={}".format(avg_time_per_epoch_secs, num_threads))
+
 
